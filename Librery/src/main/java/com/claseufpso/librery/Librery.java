73b8c1c5067a56b6de8b3f5cb1fef12ff86b0c25
cmquintero <cmquinterot@fpso.edu.co>
2024-03-06 14:44:23 -0500

    creacion libreria

---
+++ Librery/src/main/java/com/claseufpso/librery/Librery.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.claseufpso.librery;
+
+/**
+ *
+ * @author SCIS2PC21
+ */
+public class Librery {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
